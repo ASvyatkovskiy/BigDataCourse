ab5a66afce6f8749bc987ca8aa74bd23b394e77a
alexey svyatkovskiy <alexeys@nat-oitwireless-inside-vapornet100-b-2361.Princeton.EDU>
2016-04-03 16:11:25 -0400

    Transformations and actions

---
+++ 1_TransformationsActions/slurm.cmd
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH -t 00:05:00
+#SBATCH --ntasks-per-node 2
+#SBATCH --cpus-per-task 3
+#SBATCH --reservation root_21
+
+module load python
+module load spark/hadoop2.6/1.6.1 
+spark-start
+echo $MASTER
+
+spark-submit --total-executor-cores 6 exercise.py 
