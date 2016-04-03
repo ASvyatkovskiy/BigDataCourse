55d2723a081cc36bdb330374dcd047e0a23170c6
alexey svyatkovskiy <alexeys@nat-oitwireless-inside-vapornet100-b-2361.Princeton.EDU>
2016-04-03 16:10:05 -0400

    Restructure

--- preexercise/hello_spark_slurm.cmd
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#SBATCH -N 1
-#SBATCH -t 00:05:00
-#SBATCH --ntasks-per-node 1
-
-module load spark/hadoop2.6/1.4.1
-spark-start
-echo $MASTER
--- scala_spark/mkdirs.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-mkdir -p src/{main,test}/{resources,scala}
-mkdir lib project
