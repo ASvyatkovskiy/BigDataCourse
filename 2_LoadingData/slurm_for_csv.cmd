5565b798d9ac4c62bc5b63cf9907459643c78628
alexey svyatkovskiy <asvyatkovskiy@gmail.com>
2016-04-03 21:12:42 -0400

    Loading data into RDD and Dataframe

---
+++ 2_LoadingData/slurm_for_csv.cmd
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH -t 00:05:00
+#SBATCH --ntasks-per-node 2
+#SBATCH --cpus-per-task 3
+#SBATCH --reservation root_21
+
+module load spark/hadoop2.6/1.6.1
+spark-start
+echo $MASTER
+
+spark-submit --total-executor-cores 6 --jars $SPARK_HOME/lib/spark-csv_2.10-1.3.0.jar,$SPARK_HOME/lib/commons-csv-1.2.jar load_csv_exercise.py
