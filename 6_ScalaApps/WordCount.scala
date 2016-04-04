a2f3e19ce383f3ad324f7739e23f1256e357b146
Alexey Svyatkovskiy <asvyatkovskiy@gmail.com>
2016-04-04 11:18:31 -0400

    Fix several bugs

--- 6_ScalaApps/WordCount.scala
+++ 6_ScalaApps/WordCount.scala
@@ -1,26 +1,21 @@
 import org.apache.spark.{SparkConf, SparkContext, SparkFiles}
 import org.apache.spark.SparkContext._
 
-/** Computes an approximation to pi */
 object WordCount {
   def main(args: Array[String]) {
 
-    val conf = new SparkConf().setAppName("Spark Pi")
+    val conf = new SparkConf().setAppName("WordCountApp")
     val spark = new SparkContext(conf)
 
-    val prefix = "/user/alexeys/MaryTests/"
-    val scheduler = Set(("CO","2004"),("CO","2005"),("NJ","2000"),("NJ","2002"),("IN","2003"))
+    val ipath_str = sys.env("WORKDIR_ROOT") 
+    val textFile = spark.textFile("file://"+ipath_str+"/2_LoadingData/unstructured/",10)
+    val counts = textFile.flatMap(line => line.split(" "))
+                 .map(word => (word, 1))
+                 .reduceByKey(_ + _)
 
-    val t0 = System.nanoTime()
-
-    for (stateyear1 <- scheduler) {
-       val model_folder = prefix+"/bills/lexs/text2b/"+stateyear1._1+"/"+stateyear1._2+"/catalog_"+stateyear1._1+stateyear1._2
-       val counts = spark.textFile(model_folder).flatMap(line => line.split(" ")).map(word => (word, 1)).reduceByKey(_ + _)
-       counts.saveAsTextFile(prefix+"/matches/"+stateyear1._1+stateyear1._2)
-    }
-
-    val t1 = System.nanoTime()
-    println("Elapsed time: " + (t1 - t0)/1000000000.)
+    println("\nTaking the 10 most frequent words in the text and corresponding frequencies:")
+    println(counts.takeOrdered(10)(Ordering[Int].reverse.on(x=>x._2)))
+        
     spark.stop()
   }
 }
