3f3aad0ff946accbf238fad72e6a4fcbc55d76b6
alexey svyatkovskiy <asvyatkovskiy@gmail.com>
2016-04-04 11:36:51 -0400

    Image processing with Thunder example

---
+++ 7_ImageProc/slurm.cmd
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#SBATCH -N 1
+#SBATCH -t 00:20:00
+#SBATCH --ntasks-per-node 6
+#SBATCH --cpus-per-task 3
+#SBATCH --reservation root_21
+
+module load spark/hadoop2.6/1.6.1
+spark-start
+echo $MASTER
+
+spark-submit --total-executor-cores 18 ml_pipeline.py
