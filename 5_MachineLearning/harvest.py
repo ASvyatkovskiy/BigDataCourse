33c57cde7c176906a5e02a97a389bfc791cf9622
alexey svyatkovskiy <asvyatkovskiy@gmail.com>
2016-04-04 11:28:46 -0400

    No longer used

--- 5_MachineLearning/harvest.py
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env python
-
-import glob
-from subprocess import Popen
-
-logfiles = glob.glob("slurm*out")
-map(lambda fname: Popen("cat "+fname+"| grep Elapsed",shell=True).wait(),logfiles)
