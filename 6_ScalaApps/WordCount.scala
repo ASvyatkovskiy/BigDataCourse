d039082a023bf12a41019cfc9b9002fcaa5c36ce
Alexey Svyatkovskiy <asvyatkovskiy@gmail.com>
2016-04-08 20:44:56 -0400

    fix input path

--- 6_ScalaApps/WordCount.scala
+++ 6_ScalaApps/WordCount.scala
@@ -7,8 +7,7 @@ object WordCount {
     val conf = new SparkConf().setAppName("WordCountApp")
     val spark = new SparkContext(conf)
 
-    val ipath_str = sys.env("WORKDIR_ROOT") 
-    val textFile = spark.textFile("file://"+ipath_str+"/2_LoadingData/unstructured/",10)
+    val textFile = spark.textFile("file:///scratch/network/alexeys/BigDataCourse/unstructured/",10)
     val counts = textFile.flatMap(line => line.split(" "))
                  .map(word => (word, 1))
                  .reduceByKey(_ + _)
