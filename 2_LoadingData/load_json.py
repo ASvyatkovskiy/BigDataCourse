7a3d3b941a3860201628150bbb1acdb16659bbbc
Alexey Svyatkovskiy <asvyatkovskiy@gmail.com>
2016-10-18 13:28:41 -0400

    Update to Spark 2.0

--- 2_LoadingData/load_json.py
+++ 2_LoadingData/load_json.py
@@ -1,26 +1,24 @@
-from pyspark import SparkContext
-from pyspark.sql import SQLContext
+from pyspark.sql import SparkSession
 
 import json
 import sys
 import os
 
 def main_sqlcontext(args):
-    sc = SparkContext(appName="LoadJson")
-    sqlContext = SQLContext(sc)
+    spark = SparkSession.builder.appName("LoadJson").getOrCreate()
 
-    input = sqlContext.read.json(os.environ.get('SCRATCH_PATH')+"/json/")
+    input = spark.read.json(os.environ.get('SCRATCH_PATH')+"/json/")
     input.printSchema()
     input.registerTempTable("movies")
-    answer = sqlContext.sql("SELECT * FROM movies WHERE title LIKE '%Atlas%'")
-    answer.repartition(1).write.save(os.environ.get('SCRATCH_PATH')+"/output_json1/", format="json")
+    answer = spark.sql("SELECT * FROM movies WHERE title LIKE '%Atlas%'")
+    answer.coalesce(1).write.save(os.environ.get('SCRATCH_PATH')+"/output_json1/", format="json")
 
 def main_unstructured(args):
-    sc = SparkContext(appName="LoadJson")
+    spark = SparkSession.builder.appName("LoadJson").getOrCreate()
 
-    input = sc.textFile(os.environ.get('SCRATCH_PATH')+"/json/")
+    input = spark.sparkContext.textFile(os.environ.get('SCRATCH_PATH')+"/json/")
     data = input.map(lambda x: json.loads(x)).filter(lambda x: 'Atlas' in x['title'])
-    data.repartition(1).saveAsTextFile(os.environ.get('SCRATCH_PATH')+"/output_json2")
+    data.coalesce(1).saveAsTextFile(os.environ.get('SCRATCH_PATH')+"/output_json2")
 
 if __name__ == "__main__":
     main_sqlcontext(sys.argv)
